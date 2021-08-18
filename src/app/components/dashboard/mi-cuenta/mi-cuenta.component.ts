60200c6452d600b48efbf107c2cb23128ef7edc7
Felipe Vanegas <j_vanegas@javeriana.edu.co>
2021-08-18 14:57:43 -0500

    funcionalidad de cargar imagen de escritorio

--- src/app/components/dashboard/mi-cuenta/mi-cuenta.component.ts
+++ src/app/components/dashboard/mi-cuenta/mi-cuenta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mi-cuenta',
@@ -18,9 +19,10 @@ export class MiCuentaComponent implements OnInit {
   '5.Usme', '6. Tunjuelito', '7.Bosa', '8.Kennedy','9.Fontibón','10.Engativá','11.Suba','12.Barrios Unidos',
   '13.Teusaquillo','14.Los Mártires', '15.Antonio Nariño', '16.Puente Aranda', '17.Candelaria',
   '18.Rafael Uribe Uribe','19.Ciudad Bolivar','20.Sumapaz'];
-
-  constructor() { }
-
+  constructor(private sanitizer: DomSanitizer) { }
+  public archivos: any = [];
+  public previsualizacion: string | undefined;
+  public bloqueado: boolean | undefined;
   ngOnInit(): void {
     this.nombreEncargado = 'Juan Felipe';
     this.apellidosEncargado = 'Vanegas Patiño';
@@ -28,5 +30,37 @@ export class MiCuentaComponent implements OnInit {
     this.correo = 'j_vanegas@javeriana.edu.co';
     this.numCelular = 3205586237;
     this.contrasena = '12345';
+    this.previsualizacion = "../../../assets/Images/pet-hotel.png"
   }
+  onFileInput(event): any
+  {
+    const archivo = event.target.files[0];
+    this.archivos.push(archivo);
+    this.extraerBase64(archivo).then((imagen: any) => 
+    {
+      this.previsualizacion = imagen.base;
+    });
+  }
+  extraerBase64 = async ($event: any) => new Promise((resolve) => {
+    try{
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base:null
+        });
+      };
+      return resolve;
+    }catch(e)
+    {
+      return null;
+    }
+  })
 }
