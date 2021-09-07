bdf0d4e96aa71814520adcec91f176d8b242b1fe
Felipe Vanegas <j_vanegas@javeriana.edu.co>
2021-09-07 16:43:41 -0500

    mapa en dashboard-adoptante

--- src/app/components/dashboard/inicio/inicio.component.ts
+++ src/app/components/dashboard/inicio/inicio.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from "@googlemaps/js-api-loader"
-import { CoordenadasService } from 'src/app/services/coordenadas.service';
 import { MouseEvent as AGMMouseEvent } from '@agm/core';
-export interface Coordenada 
-{
-  latitude: number |any;
-  longitude: number |any;
+export interface Coordenada {
+  latitude: number | any;
+  longitude: number | any;
 }
+var activarPuntos: boolean = false;
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -15,32 +14,38 @@ export interface Coordenada
 
 export class InicioComponent implements OnInit {
   geolocalizacion: google.maps.LatLng | any;
-  latitude: number |any;
-  longitude: number |any;
-  zoom : number = 20;
+  latitude: number | any;
+  longitude: number | any;
+  zoom: number = 20;
   coordenadas: Coordenada[] = [];
-  coord : Coordenada |any;
-  coordenada1: number |any
-  constructor() { 
+  coord: Coordenada | any;
+  coordenada1: number | any
+  constructor() {
   }
   ngOnInit(): void {
     this.geolocalizar();
   }
-  geolocalizar()
-  {
-    if('geolocation' in navigator)
-    {
+  geolocalizar() {
+    if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
       });
     }
   }
-  mapClicked($event: AGMMouseEvent)
-  {
+  mapClicked($event: AGMMouseEvent) {
+    if (activarPuntos) {
+      const coordinates: Coordenada = { latitude: $event.coords.lat, longitude: $event.coords.lng }
+      this.coordenadas.push(coordinates);
+      console.log('coordenadaSize ' + this.coordenadas.length);
+    }
 
-    const coordinates: Coordenada = {latitude : $event.coords.lat, longitude : $event.coords.lng}
-    this.coordenadas.push(coordinates);
-    console.log('coordenadaSize' + this.coordenadas.length);
+  }
+  activarPuntos() {
+    activarPuntos = true;
+  }
+  desactivarPuntos()
+  {
+    activarPuntos = false;
   }
 }
