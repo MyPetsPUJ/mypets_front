12f0e023e269758bb773e3f79fc2075333842c23
Felipe Vanegas <j_vanegas@javeriana.edu.co>
2021-09-07 16:06:34 -0500

    puntos de interés

--- src/app/components/dashboard/inicio/inicio.component.ts
+++ src/app/components/dashboard/inicio/inicio.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from "@googlemaps/js-api-loader"
+import { CoordenadasService } from 'src/app/services/coordenadas.service';
+import { MouseEvent as AGMMouseEvent } from '@agm/core';
+export interface Coordenada 
+{
+  latitude: number |any;
+  longitude: number |any;
+}
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
+
 export class InicioComponent implements OnInit {
   geolocalizacion: google.maps.LatLng | any;
+  latitude: number |any;
+  longitude: number |any;
+  zoom : number = 20;
+  coordenadas: Coordenada[] = [];
+  coord : Coordenada |any;
+  coordenada1: number |any
   constructor() { 
   }
   ngOnInit(): void {
+    this.geolocalizar();
   }
+  geolocalizar()
+  {
+    if('geolocation' in navigator)
+    {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    }
+  }
+  mapClicked($event: AGMMouseEvent)
+  {
 
+    const coordinates: Coordenada = {latitude : $event.coords.lat, longitude : $event.coords.lng}
+    this.coordenadas.push(coordinates);
+    console.log('coordenadaSize' + this.coordenadas.length);
+  }
 }
---
+++ src/app/services/coordenadas.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoordenadasService {
+  latitud: number |any;
+  longitud: number |any;
+  
+}
