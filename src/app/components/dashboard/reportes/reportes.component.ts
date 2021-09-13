fdb2aed75e6e548cdd7db9fabc817a3110046067
Felipe Vanegas <j_vanegas@javeriana.edu.co>
2021-09-13 15:11:30 -0500

    Pantalla reportes

--- src/app/components/dashboard/reportes/reportes.component.ts
+++ src/app/components/dashboard/reportes/reportes.component.ts
@@ -1,4 +1,9 @@
+import { DatePipe } from '@angular/common';
+import { HOST_ATTR } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { stringify } from 'querystring';
+
 
 @Component({
   selector: 'app-reportes',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reportes.component.css']
 })
 export class ReportesComponent implements OnInit {
-
-  constructor() { }
+  range = new FormGroup({
+    inicio: new FormControl(),
+    final: new FormControl()
+  });
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
-
+  verFecha()
+  {
+    console.log(this.range.value);
+  }
 }
