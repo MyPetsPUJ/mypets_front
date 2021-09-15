5c6b890c34289983c1969e1c4abbb78caab97236
Juan Camilo <juancamilo110599@gmail.com>
2021-09-15 12:14:47 -0500

    interfaces

---
+++ src/app/components/interfaces/formularioReferencia.ts
@@ -0,0 +1,8 @@
+export interface Referencias{
+    nombres: String;
+    apellidos: String;
+    numFijo: number;
+    numCelular: number;
+    parentezco: String;
+    tiempoDeConocimiento: number;
+}
