9da9b1eb1b7fb9443398127ff47cdab8f4016543
Juan Camilo <juancamilo110599@gmail.com>
2021-10-04 17:39:42 -0500

    formulario

--- src/app/components/dashboard-adoptante/solicitudes-adopcion-adoptante/formulario-adopcion/formulario-adopcion.component.ts
+++ src/app/components/dashboard-adoptante/solicitudes-adopcion-adoptante/formulario-adopcion/formulario-adopcion.component.ts
@@ -27,6 +27,9 @@ export class FormularioAdopcionComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  periodoAdaptacion:any=["Menos de dos meses", "Entre 2 y 5 meses", "Entre 5 y 12 meses", "El tiempo que sea necesario"];
+  casaDeLaMascota:any[]=["Patio","Terraza","Garaje","Dentro de la casa","Dormirá conmigo"];
+  actividadesConMascota:any[]=["Senderismo","Montañismo","Ciclovía","Llevarla regularmente al trabajo","Llevarla con usted de viaje","Llevarla con usted a hacer ejercicio","Llevarla a parques"];
   dineroEstimadoMascota:any[]=["Menos de 50 mil pesos", "Entre 50 y 150 mil pesos", "Entre 200 y 300 mil pesos","Más de 300 mil pesos"];
   gastosMascota:any[]=["Veterinario","Vacunas y desparasitación","Alimento","Accesorios(juguetes,plato,etc)","Cama","Correa","Placa de Identificación"]
   lugarDeJugar:any[]=["Patio Interior","Patio Exterior","Garaje","Zonas verdes cercanas","Parques en las zonas de domicilio"];
