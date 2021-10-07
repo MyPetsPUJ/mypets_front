099fc36c6d6f1ac3ddb51b7cd84d95dd1395503d
Juan Camilo <juancamilo110599@gmail.com>
2021-10-07 12:54:14 -0500

    navbar adoptante

--- src/app/components/dashboard-adoptante/navbar/navbar.component.ts
+++ src/app/components/dashboard-adoptante/navbar/navbar.component.ts
@@ -11,6 +11,25 @@ export class NavbarComponent implements OnInit {
   constructor(public loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.correrNavbar();
+  }
+  correrNavbar()
+  {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll<HTMLElement>('.nav-links li');
+    burger?.addEventListener('click',()=>{
+      nav?.classList.toggle('nav-active');
+      navLinks.forEach((link,index)=>{
+        if(link.style.animation){
+            link.style.animation='';
+        } else {
+            link.style.animation= `navLinkFade 0.5s ease forwards ${index/7+0.3}s`;
+        }
+      });
+      burger?.classList.toggle("toggle");
+    });
+    
   }
 
 }
