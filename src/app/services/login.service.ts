8fd3cc169d2ed54da1153cafcb09d756c4b75a92
Gabriel Niño <nino.gandres@javeriana.edu.co>
2021-10-09 14:44:45 -0500

    autologin y guardando token en localstorage

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -6,37 +6,38 @@ import { InicioSesion } from '../components/interfaces/inicioSesion';
 
 import { map } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { threadId } from 'worker_threads';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-
   private token: string = '';
   private authStatusListener = new Subject<boolean>();
   private isAuth = false;
+  private tokenTimer: NodeJS.Timer | undefined;
 
-  constructor(private http: HttpClient, private _router: Router) { }
+  constructor(private http: HttpClient, private _router: Router) {}
 
   getToken() {
     return this.token;
   }
 
-  getIsAuth(){
+  getIsAuth() {
     return this.isAuth;
   }
 
-  getAuthStatusListener(){
+  getAuthStatusListener() {
     return this.authStatusListener.asObservable();
   }
 
-  getTokenStorage(){
+  getTokenStorage() {
     return sessionStorage.getItem('token');
   }
 
   inicioSesion(inicioSesion: InicioSesion) {
     return this.http
-      .post<{ token: string }>(
+      .post<{ token: string; expiresIn: number }>(
         'http://localhost:3000/api/login',
         inicioSesion,
         { withCredentials: true }
@@ -46,23 +47,73 @@ export class LoginService {
           console.log(res);
           const token = res.token;
           this.token = token;
-          if(token){
+          if (token) {
+            const expiresInDuration = res.expiresIn;
+            this.setAuthTimer(expiresInDuration);
             this.isAuth = true;
-            sessionStorage.setItem('token', this.token);
             this.authStatusListener.next(true);
+            const now = new Date();
+            const expirationDate = new Date(
+              now.getTime() + expiresInDuration * 1000
+            );
+            console.log(expirationDate);
+            this.saveAuthDate(token, expirationDate);
           }
-
         })
       );
     //return this.servicioBase.post([this.login], inicioSesion);
   }
 
-  logout(){
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    if(!authInformation){
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation!.expirationDate.getTime() - now.getTime();
+    if(expiresIn > 0){
+      this.token = authInformation!.token;
+      this.isAuth = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
+  }
+
+  logout() {
     this.token = '';
     this.isAuth = false;
     this.authStatusListener.next(false);
-    sessionStorage.removeItem('token');
+    clearTimeout(this.tokenTimer!);
+    this.clearAuthData();
     this._router.navigate(['/login']);
   }
 
+  private setAuthTimer(duration: number){
+    console.log("Setting timer: " + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
+  private saveAuthDate(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate),
+    };
+  }
 }
