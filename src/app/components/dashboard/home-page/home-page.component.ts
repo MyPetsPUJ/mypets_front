2c6688a8850e24b33b4fc435c56e05069b746bf4
Juan Camilo <juancamilo110599@gmail.com>
2021-10-15 16:45:40 -0500

    home

--- src/app/components/dashboard/home-page/home-page.component.ts
+++ src/app/components/dashboard/home-page/home-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { EntidadAnimal } from '../../interfaces/usuarios/entidadAnimal';
+import { AnimalService } from 'src/app/services/animal/animal.service';
+import { MatDialog } from '@angular/material/dialog';
+
+
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  users: string[] = ['Felipe Perez','Andres Castro','Felipe Vanegas'];
+  animales: EntidadAnimal[] = [];
+  columnas: number=this.animales.length;
+  nombreFundacion: string | undefined;
+  logoFundacion: string | undefined;
+  
 
-  constructor() { }
+  constructor(private animalService: AnimalService,  public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.animalService.getAnimales().subscribe({
+      next: (res) => {
+        console.log(res)
+        this.animales = res;
+        console.log("animales", this.animales)
+      
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+    
   }
-
+ 
 }
