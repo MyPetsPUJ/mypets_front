7b71bdb88c39efbd0f25d302fc78082f4de302bf
Juan Camilo <juancamilo110599@gmail.com>
2021-10-16 22:04:07 -0500

    home

--- src/app/components/dashboard/home-page/home-page.component.ts
+++ src/app/components/dashboard/home-page/home-page.component.ts
@@ -13,7 +13,10 @@ import { MatDialog } from '@angular/material/dialog';
 export class HomePageComponent implements OnInit {
   users: string[] = ['Felipe Perez','Andres Castro','Felipe Vanegas'];
   animales: EntidadAnimal[] = [];
-  columnas: number=this.animales.length;
+  columnas:number=this.animales.length;
+  valoresColumna1:number[]=[];
+  valoresColumna2:number[]=[];
+  valoresColumna3:number[]=[];
   nombreFundacion: string | undefined;
   logoFundacion: string | undefined;
   
@@ -33,6 +36,34 @@ export class HomePageComponent implements OnInit {
       }
     });
     
+  }
+  dividirColumnas(): void{
+    for(var i=1;i<=this.animales.length;i+3)
+    { 
+      this.valoresColumna1.push(i);
+      this.valoresColumna2.push(i+2);
+      this.valoresColumna3.push(i+3);
+
+      if(this.animales.length - i < 3 )
+      {
+        this.valoresColumna2.push(i+4)
+        this.valoresColumna3.push(i+5)
+        this.valoresColumna1.push(i+6)
+        break;
+      }
+      if(this.animales.length - i < 2 )
+      {
+        this.valoresColumna2.push(i+4)
+        this.valoresColumna1.push(i+5)
+        break;
+      }
+      if(this.animales.length - i < 1 )
+      {
+        this.valoresColumna2.push(i+4)
+        break;
+      }
+      
+    }
   }
  
 }
