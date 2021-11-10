620443cefa4061772b29a334830e71a5d097d4b5
Ricardo Riscanevo <46947724+Ricardo-Riscanevo-1@users.noreply.github.com>
2021-11-10 00:36:45 -0500

    Revision error v1

--- src/app/services/adopcion/enviar-solicitud-adopcion.service.ts
+++ src/app/services/adopcion/enviar-solicitud-adopcion.service.ts
@@ -83,9 +83,12 @@ export class EnviarSolicitudAdopcionService {
   actualizarEstadoSolicitud(id: string,estado: string)
   {
     const body = { estado: estado };
-    return this.http.put<any>(
+    return this.http.put(
       `http://${this.dominio}:${this.puerto}/${this.pathIntermedio}/${this.entidad}/${this.subTipoEntidadII}/${id}`,body
-    );
+    ).subscribe(res=>
+      {
+        console.log('Respuesta: ',res)
+      })
   }
 
   addSolicitud(solicitud: EntidadSolicitudAdopcion)
