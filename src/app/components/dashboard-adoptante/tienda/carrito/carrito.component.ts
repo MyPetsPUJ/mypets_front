36b1b09ff8be5e1f7ba1de9f01b6e125214aebd6
Juan Camilo <juancamilo110599@gmail.com>
2021-11-15 20:20:12 -0500

    cambios navbar

--- src/app/components/dashboard-adoptante/tienda/carrito/carrito.component.ts
+++ src/app/components/dashboard-adoptante/tienda/carrito/carrito.component.ts
@@ -3,6 +3,7 @@ import { Producto } from 'src/app/components/interfaces/tienda/entidadProducto';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { threadId } from 'worker_threads';
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
@@ -10,20 +11,28 @@ import { Subscription } from 'rxjs';
 })
 
 export class CarritoComponent implements OnInit {
-  productos: Producto[]=this.data.animales
+  productos: Producto[]=this.data.animales;
+  sumarProductos: number=0;
   constructor(
   @Inject(MAT_DIALOG_DATA) public data: any,
   public dialogRef: MatDialogRef<CarritoComponent>,
   private _router: Router) { }
 
   ngOnInit(): void {
-    
+    this.sumar()
   }
   accion(nombre: string) {
     if (nombre == 'cerrar') {
       this.dialogRef.close();
     }
   }
+  sumar(){
+    for(var i=0;i<this.productos.length;i++)
+      {
+        this.sumarProductos=this.sumarProductos+parseInt(this.productos[i].precio)
+      }
+  }
+  
   
 
 }
